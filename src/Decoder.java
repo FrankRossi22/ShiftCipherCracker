5cb448a16295e92980f15043bbee202af4c2b16a
Frank Rossi <rossi.229@osu.edu>
2025-09-04 15:05:15 -0400

    fixed mispellings

--- src/Decoder.java
+++ src/Decoder.java
@@ -2,48 +2,54 @@
 public class Decoder {
     public static void main(String[] args) {
 
-        String cypherText = "BIVHA JVTTH UKLYZ DLYLA YHPUL KAVSP UNLYV UAOLJ VUCVF ZWYVQ\n" +
-                "LJALK SPULV MHKCH UJLHA AOLSP TPAVM CPZPI PSPAF BUAPS AOLMH\n" +
-                "SSVMK HYRAO LUAVZ BYMHJ LPMWV ZZPIS LDPAO PUAOL JVUCV FJVSB\n" +
-                "TUZMP YLZHS CVLZM YVTIV DHUKZ ALYUA BILZZ PTBSA HULVB ZSFHU\n" +
-                "KAVZB ITLYN LHUKT HRLAO LPYLZ JHWLH ZAOLL ZJVYA ZHWWL HYLKA\n" +
-                "OLKPM MPJBS AFMVY AOLBI VHAZD HZAVS VJHAL HJVUC VFPUA OLMPY\n" +
-                "ZAWSH JLAOL SPTPA VMCPZ PIPSP AFHAI LZAMY VTHJV UUPUN AVDLY\n" +
-                "DHZAL UTPSL ZHWHA YVSSP ULVMA LUBIV HAZJV BSKAO LYLMV YLJVC\n" +
-                "LYADV OBUKY LKADL UAFTP SLZVM ZLHHJ VUCVF VMMPM AFZOP WZTLH\n" +
-                "UDOPS LVJJB WPLKH MYVUA VMVUS FADLU AFMVB YOBUK YLKFH YKZPU\n" +
-                "AOLLU VYTVB ZZWHJ LZVMA OLHAS HUAPJ HASLH ZAUPU LTPSS PVUZX\n" +
-                "BHYLT PSLZV MVWLY HAPVU HSDHA LYZAO LVULJ VBSKI LTPZZ LKIFA\n" +
-                "OLVAO LYDPA OAOLN YLHAL ZALHZ LHUKB ZBHSS FDHZI HKDLH AOLYW\n" +
-                "SHFLK HWHYA OPKPU NAOLJ VUCVF ZMYVT NLYTH ULFLZ VYMVY JPUNB\n" +
-                "IVHAZ AVZLL RZOLS ALYMY VTAOL LSLTL UAZHA WLYPZ JVWLK LWAOZ\n" +
-                "VKPKY VBAPU LHSAL YHAPV UZVMJ VBYZL HUKLT LYNLU JFABY UZPMJ\n" +
-                "VUAHJ ADLYL THKLK LSPIL YHALY LYVBA PUNVM JVUCV FZOVD LCLYH\n" +
-                "DHFMY VTWHA YVSSP ULZHU KDVSM WHJRZ SVJHA LKIFI SLAJO SLFDH\n" +
-                "ZAOLT VZAWY VKBJA PCLTL AOVKK HCPKR HOUAO LNYLH AOPZA VYPHU\n" +
-                "VMJYF WAVNY HWOFN PCLZH KYHTH APJHJ JVBUA VMVUL ZBJOY LYVBA\n" +
-                "PUNJV UALZA PUOPZ IVVRV UAOLB IVHAD HYZLP GPUNA OLLUP NTHAO\n" +
-                "PZWHZ ZHNLL EJLYW ALKMY VTQVO URLLN HUPUA LSSPN LUJLP UDHYY";
+        String cipherText = """
+                BIVHA JVTTH UKLYZ DLYLA YHPUL KAVSP UNLYV UAOLJ VUCVF ZWYVQ
+                LJALK SPULV MHKCH UJLHA AOLSP TPAVM CPZPI PSPAF BUAPS AOLMH
+                SSVMK HYRAO LUAVZ BYMHJ LPMWV ZZPIS LDPAO PUAOL JVUCV FJVSB
+                TUZMP YLZHS CVLZM YVTIV DHUKZ ALYUA BILZZ PTBSA HULVB ZSFHU
+                KAVZB ITLYN LHUKT HRLAO LPYLZ JHWLH ZAOLL ZJVYA ZHWWL HYLKA
+                OLKPM MPJBS AFMVY AOLBI VHAZD HZAVS VJHAL HJVUC VFPUA OLMPY
+                ZAWSH JLAOL SPTPA VMCPZ PIPSP AFHAI LZAMY VTHJV UUPUN AVDLY
+                DHZAL UTPSL ZHWHA YVSSP ULVMA LUBIV HAZJV BSKAO LYLMV YLJVC
+                LYADV OBUKY LKADL UAFTP SLZVM ZLHHJ VUCVF VMMPM AFZOP WZTLH
+                UDOPS LVJJB WPLKH MYVUA VMVUS FADLU AFMVB YOBUK YLKFH YKZPU
+                AOLLU VYTVB ZZWHJ LZVMA OLHAS HUAPJ HASLH ZAUPU LTPSS PVUZX
+                BHYLT PSLZV MVWLY HAPVU HSDHA LYZAO LVULJ VBSKI LTPZZ LKIFA
+                OLVAO LYDPA OAOLN YLHAL ZALHZ LHUKB ZBHSS FDHZI HKDLH AOLYW
+                SHFLK HWHYA OPKPU NAOLJ VUCVF ZMYVT NLYTH ULFLZ VYMVY JPUNB
+                IVHAZ AVZLL RZOLS ALYMY VTAOL LSLTL UAZHA WLYPZ JVWLK LWAOZ
+                VKPKY VBAPU LHSAL YHAPV UZVMJ VBYZL HUKLT LYNLU JFABY UZPMJ
+                VUAHJ ADLYL THKLK LSPIL YHALY LYVBA PUNVM JVUCV FZOVD LCLYH
+                DHFMY VTWHA YVSSP ULZHU KDVSM WHJRZ SVJHA LKIFI SLAJO SLFDH
+                ZAOLT VZAWY VKBJA PCLTL AOVKK HCPKR HOUAO LNYLH AOPZA VYPHU
+                VMJYF WAVNY HWOFN PCLZH KYHTH APJHJ JVBUA VMVUL ZBJOY LYVBA
+                PUNJV UALZA PUOPZ IVVRV UAOLB IVHAD HYZLP GPUNA OLLUP NTHAO
+                PZWHZ ZHNLL EJLYW ALKMY VTQVO URLLN HUPUA LSSPN LUJLP UDHYY""";
 
         //Set the number of times to shift each letter (excluding ' ' and '\n')
         int shift = 19;
+        String decodedText = decodeShiftcipher(cipherText, shift);
+
+        System.out.println("Decoded text with " + shift + " shifts: " + decodedText);
+
+    }
+
+    private static String decodeShiftcipher(String cipherText, int shift) {
         String decodedText = "";
 
-        //for loop to go through each character in the cypherText, shift it, and then add it to the decodedText
-        for (int i = 0; i < cypherText.length(); i++) {
-            if(cypherText.charAt(i) == ' ' || cypherText.charAt(i) == '\n') {
-                decodedText += cypherText.charAt(i);
+        //for loop to go through each character in the cipherText, shift it, and then add it to the decodedText
+        for (int i = 0; i < cipherText.length(); i++) {
+            if(cipherText.charAt(i) == ' ' || cipherText.charAt(i) == '\n') {
+                decodedText += cipherText.charAt(i);
                 continue;
             }
             // Get the character value (0-25) by subtracting 'A', apply the shift, and convert back to a character
-            int charVal = cypherText.charAt(i) - 'A';
+            int charVal = cipherText.charAt(i) - 'A';
             charVal = (charVal + shift) % 26; // Shift forward by j positions
             char decodedChar = (char) (charVal + 'A');
             decodedText += decodedChar;
 
         }
-
-        System.out.println("Decoded text with " + shift + " shifts: " + decodedText);
-
+        return decodedText;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Set the cypher word to decode
-        String cypherWord = "BIVHA JVTTH";
+        //Set the cipher word to decode
+        String cipherWord = "BIVHA JVTTH";
 
         //Try all possible shifts from 1 to 26
         for(int j = 1; j <= 26; j++) {
             String decodedWord = "";
 
-            //for loop to go through each character in the cypherWord, shift it, and then add it to the decodedWord
-            for (int i = 0; i < cypherWord.length(); i++) {
-                if(cypherWord.charAt(i) == ' ' || cypherWord.charAt(i) == '\n') {
-                    decodedWord += cypherWord.charAt(i);
+            //for loop to go through each character in the cipherWord, shift it, and then add it to the decodedWord
+            for (int i = 0; i < cipherWord.length(); i++) {
+                if(cipherWord.charAt(i) == ' ' || cipherWord.charAt(i) == '\n') {
+                    decodedWord += cipherWord.charAt(i);
                     continue;
                 }
                 // Get the character value (0-25) by subtracting 'A', apply the shift, and convert back to a character
-                int charVal = cypherWord.charAt(i) - 'A';
+                int charVal = cipherWord.charAt(i) - 'A';
                 charVal = (charVal + j) % 26; // Shift back by j positions
                 char decodedChar = (char) (charVal + 'A');
                 decodedWord += decodedChar;
